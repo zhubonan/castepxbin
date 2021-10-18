c86cbd41f834b914fb1acabebe0e214cf0ad367e
Matthew Evans <git@ml-evs.science>
2021-10-18 16:41:50 +0100

    Add placeholder test

--- castepxbin/test_reader.py
+++ castepxbin/test_reader.py
@@ -6,6 +6,7 @@ import pytest
 import numpy as np
 
 from .pdos import read_pdos_bin, reorder_pdos_data, compute_pdos
+from .castep_bin import read_castep_bin
 from pymatgen.electronic_structure.core import Orbital, Spin
 
 
@@ -17,6 +18,11 @@ def pdos_bin():
 @pytest.fixture
 def bands_file():
     return os.path.join(os.path.split(__file__)[0], 'test_data/Si2.bands')
+
+
+@pytest.fixture
+def castep_bin():
+    return os.path.join(os.path.split(__file__)[0], 'test_data/Si2.castep_bin')
 
 
 def test_pdos_reader(pdos_bin):
@@ -46,3 +52,16 @@ def test_pdos_compute(pdos_bin, bands_file):
     bins = np.arange(0.0, 10.0 + bin_width, bin_width)
 
     pdos = compute_pdos(pdos_bin, eigenvalues, weights, bins)
+
+
+@pytest.mark.skipif(not os.isfile(castep_bin))
+def test_castep_bin_reader(castep_bin):
+    data = read_castep_bin(castep_bin, records_to_extract=("FORCES", "FORCE_CON"))
+    expected_fields = (
+        "forces",
+        "phonon_supercell_matrix",
+        "phonon_force_constant_matrix",
+        "phonon_supercell_origins",
+        "phonon_force_constant_row"
+    )
+    assert all(field in data for field in expected_fields)
