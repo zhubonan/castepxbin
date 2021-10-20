6711ee60bd35507cab1d3f58b0a1cdc0e8eb1ef6
Bonan Zhu <zhubonan@outlook.com>
2021-10-20 21:34:44 +0100

    Update the top level imports

--- castepxbin/__init__.py
+++ castepxbin/__init__.py
@@ -3,3 +3,4 @@ Collection of binary file readers for CASTEP
 """
 from .pdos import compute_pdos, read_pdos_bin
 from .castep_bin import read_castep_bin
+from .ome_bin import read_dome_bin, read_cst_ome, read_ome_bin
