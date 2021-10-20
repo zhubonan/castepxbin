63c3f52f94ad688f2cb7ac8790876eb4a77fb764
Bonan Zhu <zhubonan@outlook.com>
2021-10-20 21:35:27 +0100

    Update the version number in setup.py and __init__.py

--- castepxbin/__init__.py
+++ castepxbin/__init__.py
@@ -4,3 +4,5 @@ Collection of binary file readers for CASTEP
 from .pdos import compute_pdos, read_pdos_bin
 from .castep_bin import read_castep_bin
 from .ome_bin import read_dome_bin, read_cst_ome, read_ome_bin
+
+__version__ = "0.2.0"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os import path
 import os
 
-version = '0.1.0'
+version = '0.2.0'
 if __name__ == '__main__':
 
     README_PATH = path.join(path.dirname(__file__), "README.md")
